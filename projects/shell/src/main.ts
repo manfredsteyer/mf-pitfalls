9a423401303e561789b25fface79e02cfa7de687
Manfred Steyer <manfred.steyer@gmx.net>
2021-07-18 15:27:51 +0200

    don't load remoteEntry upfront to provoke a version mismatch

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,7 +1,7 @@
 import { loadRemoteEntry } from '@angular-architects/module-federation';
 
 Promise.all([
-   loadRemoteEntry('https://brave-plant-03ca65b10.azurestaticapps.net/remoteEntry.js', 'mfe1')
+  // loadRemoteEntry('https://brave-plant-03ca65b10.azurestaticapps.net/remoteEntry.js', 'mfe1')
 ])
 .catch(err => console.error('Error loading remote entries', err))
 .then(() => import('./bootstrap'))
